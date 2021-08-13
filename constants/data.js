11f7a6703ef3f6965fd3f318ea0bacc6a598125a
nagendragpu <nagendragpu@gmail.com>
2021-08-14 02:20:25 +0530

    data

--- constants/data.js
+++ constants/data.js
@@ -8,8 +8,8 @@ export const data = [
   {
     id: 2,
     name: 'nagendra2',
-    title: 'Thank you Tokyo',
-    content: `COVID-19 has torn a particularly lethal path through diabetics across the world, especially in India and the United States. People with diabetes account for a staggering 40% of those who have died from COVID-19 in the US, and the numbers from India too indicate a similar pattern. The greater concern now is that many patients who have recovered from COVID-19 have developed newly diagnosed, full-blown diabetes. `,
+    title: 'Afghanistan is about to fall to the Taliban',
+    content: `The Taliban have continued their rout of Afghan forces, seizing control of Kandahar, Herat and Lashkar Gah in quick succession and moving dangerously close to the capital city of Kabul. The speed of the cities’ collapse, combined with American officials’ announcement that they would evacuate most of the United States Embassy, has deepened the sense of panic across the country. President Joe Biden is keeping a low profile as the world prepares to pay the price of his administration’s premature withdrawal. `,
   },
   {
     id: 3,
