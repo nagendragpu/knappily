99c6501de6657d6920a2034bb7c5ee232480d59d
nagendragpu <nagendragpu@gmail.com>
2021-08-16 03:10:08 +0530

    scroll indicator animating

--- screens/Detail.js
+++ screens/Detail.js
@@ -2,29 +2,47 @@ import React from 'react';
 import {
   Dimensions,
   FlatList,
+  Image,
   SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   View,
+  Animated,
 } from 'react-native';
 import {useRoute} from '@react-navigation/native';
 const {width, height} = Dimensions.get('window');
+const RED_COLOR = '#7c1518';
 
 const Detail = () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
   const route = useRoute();
   //   console.log(route.params);
   const data = route.params.detail;
-  //   console.log(dataR);
+  // console.log(data);
+
+  const scrollView = React.useRef();
 
   function renderHeader() {
     return (
-      <View style={{height: 60}}>
+      <View
+        style={{
+          height: 65,
+          backgroundColor: '#d0d0d0',
+          padding: 8,
+          paddingBottom: 2,
+          // borderWidth: 2,
+          // borderColor: 'red',
+        }}>
         <ScrollView
+          ref={scrollView}
           horizontal={true}
           showsVerticalScrollIndicator={false}
+          // onScroll={event => {
+          //   console.log(event.nativeEvent.contentOffset.x);
+          // }}
           style={{}}
-          contentContainerStyle={{margin: 5}}>
+          contentContainerStyle={{}}>
           <View
             style={{
               // height: 100,
@@ -35,7 +53,7 @@ const Detail = () => {
               alignItems: 'center',
               backgroundColor: 'white',
             }}>
-            <View style={styles.box}>
+            <View style={[styles.box]}>
               <Text style={styles.boxContent}>WHAT</Text>
             </View>
             <View style={styles.box}>
@@ -55,33 +73,78 @@ const Detail = () => {
             </View>
           </View>
         </ScrollView>
+        <Animated.View
+          style={{
+            borderBottomWidth: 5,
+            borderBottomColor: RED_COLOR,
+            width: 80,
+            transform: [
+              {
+                translateX: scrollX.interpolate({
+                  inputRange: [0, width, 2 * width, 3 * width, 4 * width],
+                  outputRange: [0, 60, 140, 220, 300],
+                }),
+              },
+            ],
+          }}
+        />
       </View>
     );
   }
 
   return (
-    <View style={{flex: 1, backgroundColor: '#c9c9c9'}}>
-      <SafeAreaView />
-      {/* {renderHeader()} */}
+    <View style={{backgroundColor: 'white', flex: 1}}>
+      {/* <SafeAreaView /> */}
+      {renderHeader()}
       <FlatList
         data={data}
-        // snapToInterval={5}
+        onEndReachedThreshold={3}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: false},
+        )}
+        contentContainerStyle={{height}}
+        style={{flex: 1}}
+        onEndReached={() => {
+          scrollView.current.scrollTo({x: 67, y: 0, animated: true});
+        }}
         pagingEnabled
+        bounces={false}
         keyExtractor={(item, index) => item.id.toString()}
         horizontal={true}
         renderItem={({item, index}) => {
-          console.log(item.content);
+          // console.log(item.content);
           return (
-            <ScrollView>
+            <ScrollView
+              scrollEnabled={true}
+              contentContainerStyle={{}}
+              style={{flex: 1}}>
               <View
                 style={{
-                  flex: 1,
-                  height,
+                  backgroundColor: 'white',
+                  borderWidth: 1,
+                  borderColor: 'red',
+                  // height,
                   width,
-                  backgroundColor: 'yellow',
                 }}>
-                <Text>{item.content}</Text>
+                <View>
+                  <Image
+                    source={require('../assets/images/joe.jpg')}
+                    style={{
+                      height: 300,
+                      width,
+                      resizeMode: 'cover',
+                    }}
+                  />
+                </View>
+                <View style={{flex: 1}}>
+                  <Text style={{fontWeight: '600', padding: 16, fontSize: 18}}>
+                    {item.content}
+                  </Text>
+                </View>
               </View>
+              {/* <View style={{height: 50}} /> */}
             </ScrollView>
           );
         }}
@@ -94,8 +157,8 @@ export default Detail;
 
 const styles = StyleSheet.create({
   box: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
   },
   boxContent: {
     fontWeight: '700',
