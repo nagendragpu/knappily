eb33284ebc588f6cad380a05b09034c857e54a7a
nagendragpu <nagendragpu@gmail.com>
2021-08-20 16:17:10 +0530

    Extracting Stack Navigator Component

--- App.js
+++ App.js
@@ -1,85 +1,19 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
-import {
-  CardStyleInterpolators,
-  createStackNavigator,
-} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import Main from './screens/Main';
-import Detail from './screens/Detail';
-import Easing from 'react-native/Libraries/Animated/Easing';
-
-const Stack = createStackNavigator();
+import {SIZES} from './constants';
+import StackNavigator from './Navigation/StackNavigator';
 
 const App = () => {
-  // function MyStack() {
-  //   return (
-
-  //   );
-  // }
-  const config = {
-    animation: 'spring',
-    config: {
-      stiffness: 1000,
-      damping: 500,
-      mass: 3,
-      overshootClamping: true,
-      restDisplacementThreshold: 0.01,
-      restSpeedThreshold: 0.01,
-    },
-  };
-  const closeConfig = {
-    animation: 'timing',
-    config: {
-      duration: 200,
-      easing: Easing.linear,
-    },
-  };
-
   return (
-    <SafeAreaView style={{backgroundColor: '#7c1518', flex: 1}}>
-      <StatusBar barStyle="light-content" backgroundColor="#7c1518" />
+    <SafeAreaView style={{backgroundColor: SIZES.primary, flex: 1}}>
+      <StatusBar barStyle="light-content" backgroundColor={SIZES.primary} />
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            gestureEnabled: true,
-            transitionSpec: {
-              open: config,
-              close: closeConfig,
-            },
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-          }}>
-          <Stack.Screen
-            name="Main"
-            component={Main}
-            options={{
-              headerShown: false,
-              // transitionSpec: {
-              //   open: config,
-              //   close: closeConfig,
-              // },
-            }}
-          />
-          <Stack.Screen
-            name="Detail"
-            component={Detail}
-            options={{
-              headerShown: false,
-              // transitionSpec: {
-              //   open: config,
-              //   close: config,
-              // },
-            }}
-          />
-        </Stack.Navigator>
+        <StackNavigator />
       </NavigationContainer>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default App;
---
+++ Navigation/StackNavigator.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from '@react-navigation/stack';
+
+import Easing from 'react-native/Libraries/Animated/Easing';
+
+import Main from '../screens/Main';
+import Detail from '../screens/Detail';
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  const config = {
+    animation: 'spring',
+    config: {
+      stiffness: 1000,
+      damping: 500,
+      mass: 3,
+      overshootClamping: true,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 0.01,
+    },
+  };
+  const closeConfig = {
+    animation: 'timing',
+    config: {
+      duration: 200,
+      easing: Easing.linear,
+    },
+  };
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        transitionSpec: {
+          open: config,
+          close: closeConfig,
+        },
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}>
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
